9461a5991ae13006b55057079c84f002708e0304
Vanessa Brito <nnessa.rbrito003@gmail.com>
2025-06-19 19:54:01 -0300

    Update Ex_03.java

--- src/exercicios_logica/Ex_03.java
+++ src/exercicios_logica/Ex_03.java
@@ -2,6 +2,10 @@ package exercicios_logica;
 
 import java.util.Scanner;
 
+/* 3 - Calcule a area de um retangulo de acordo com 
+ * a base e altura e exiba o resultado na tela
+ */
+
 public class Ex_03 {
 
 	public static void main(String[] args) {
